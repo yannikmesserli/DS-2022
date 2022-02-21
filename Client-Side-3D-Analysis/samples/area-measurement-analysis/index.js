91440de8d15500050ee84b46aaa6bcae747c904e
Hugo Campos <hccampos@users.noreply.github.com>
2022-02-21 20:41:21 +0000

    More analysis slides

--- Client-Side-3D-Analysis/samples/area-measurement-analysis/index.js
+++ Client-Side-3D-Analysis/samples/area-measurement-analysis/index.js
@@ -3,6 +3,9 @@ import { initView, onInit } from "../utils.js";
 
 let view, widget;
 
+// Move in once analysis objects are done:
+// https://zrh-app-sd-2.esri.com/arcgis-play/a/hcampos_zurich/devsummit-2022---areameasurementanalysis
+
 onInit("area-measurement-analysis", () => {
   view = initView();
 });
