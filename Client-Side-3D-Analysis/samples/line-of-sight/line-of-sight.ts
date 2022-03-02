81d852a452d7bef119699246fb2ac4a62877a693
Hugo Campos <hccampos@users.noreply.github.com>
2022-03-02 11:17:27 +0000

    Finalize LineOfSight sample (Munich)

--- Client-Side-3D-Analysis/samples/line-of-sight/line-of-sight.ts
+++ Client-Side-3D-Analysis/samples/line-of-sight/line-of-sight.ts
@@ -1,14 +1,14 @@
 // @ts-check
 import SceneView from "@arcgis/core/views/SceneView";
 import LineOfSight from "@arcgis/core/widgets/LineOfSight";
-import { SHADOW_CAST } from "../../../common/scenes";
+import { MUNICH } from "../../../common/scenes";
 import { initView, onInit } from "../../../common/utils";
 
 let view: SceneView;
 let widget: LineOfSight;
 
 onInit("line-of-sight", () => {
-  view = initView(SHADOW_CAST);
+  view = initView(MUNICH);
   widget = new LineOfSight({ view });
   view.ui.add(widget, "top-right");
 });
