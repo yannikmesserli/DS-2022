da9674a40d2df441ed0f8e7f5d7b0ab6c5961ebd
Hugo Campos <hccampos@users.noreply.github.com>
2022-03-04 00:34:14 +0100

    Finalize SceneLayer slide and sample

--- common/scenes.ts
+++ common/scenes.ts
@@ -5,5 +5,6 @@ export const MANHATTAN = "9a542f6755274436985617a462ffdf44";
 export const MUNICH = "cfddaf892c4b477bbc42a662acc4e88a";
 export const SAMEDAN_AIRPORT = "3ef242366dc24c2d98e3f8a0f253ae12";
 export const SAN_FRANCISCO = "c7adc0ed4c124c72ad7bc79bddada094";
+export const SAN_FRANCISCO_EDITABLE = "2e38f798f3124f2dae84217839cc43dc";
 export const SHADOW_CAST = "f2220db76c6448b4be8083d19ef4cf8d";
 export const WORLD_CAPITALS = "c025297bbf004abc863773858add4058";
--- web-editing-in-3d/samples/scene-layer/scene-layer.ts
+++ web-editing-in-3d/samples/scene-layer/scene-layer.ts
@@ -1,76 +1,36 @@
-import Camera from "@arcgis/core/Camera";
-import FieldElement from "@arcgis/core/form/elements/FieldElement";
-import FormTemplate from "@arcgis/core/form/FormTemplate";
-import SpatialReference from "@arcgis/core/geometry/SpatialReference";
-import FeatureLayer from "@arcgis/core/layers/FeatureLayer";
-import UniqueValueRenderer from "@arcgis/core/renderers/UniqueValueRenderer";
-import RotationVariable from "@arcgis/core/renderers/visualVariables/RotationVariable";
-import SizeVariable from "@arcgis/core/renderers/visualVariables/SizeVariable";
-import WebStyleSymbol from "@arcgis/core/symbols/WebStyleSymbol";
+import SimpleRenderer from "@arcgis/core/renderers/SimpleRenderer";
+import SolidEdges3D from "@arcgis/core/symbols/edges/SolidEdges3D";
+import FillSymbol3DLayer from "@arcgis/core/symbols/FillSymbol3DLayer";
+import MeshSymbol3D from "@arcgis/core/symbols/MeshSymbol3D";
 import Editor from "@arcgis/core/widgets/Editor";
-import { SAMEDAN_AIRPORT } from "../../../common/scenes";
-import { initView } from "../../../common/utils";
+import { SAN_FRANCISCO_EDITABLE } from "../../../common/scenes";
+import { initView, onPlayClick } from "../../../common/utils";
 
-init();
+(async function init(): Promise<void> {
+  const view = initView(SAN_FRANCISCO_EDITABLE);
 
-async function init(): Promise<void> {
-  const view = initView(SAMEDAN_AIRPORT);
-  view.camera = new Camera({
-    position: {
-      spatialReference: SpatialReference.WebMercator,
-      x: 1099838.309838232,
-      y: 5865770.443455763,
-      z: 1771.2705388450995,
-    },
-    heading: 327.12912283465096,
-    tilt: 46.776661717554724,
-  });
+  await view.when();
+  const sceneLayer = view.map.layers.getItemAt(0) as __esri.SceneLayer;
 
-  const [airplaneSymbol, helicopterSymbol] = await Promise.all(
-    [
-      new WebStyleSymbol({ name: "Airplane_Propeller", styleName: "EsriRealisticTransportationStyle" }),
-      new WebStyleSymbol({ name: "Eurocopter_AS-365_-_Flying", styleName: "EsriRealisticTransportationStyle" }),
-    ].map((style) => style.fetchSymbol())
-  );
-
-  const pointLayer = new FeatureLayer({
-    title: "Airplanes",
-    url: "https://services.arcgis.com/V6ZHFr6zdgNZuVG0/arcgis/rest/services/EditableFeatures3D/FeatureServer/3",
-    elevationInfo: { mode: "absolute-height" },
-    outFields: ["*"],
-    renderer: new UniqueValueRenderer({
-      field: "TYPE",
-      uniqueValueInfos: [
-        { value: "1", label: "Helicopter", symbol: helicopterSymbol },
-        { value: "2", label: "Airplane", symbol: airplaneSymbol },
-      ],
-      visualVariables: [
-        new SizeVariable({
-          axis: "height",
-          field: "SIZE",
-          valueUnit: "meters",
-        }),
-        new RotationVariable({
-          field: "ROTATION",
-          rotationType: "geographic",
+  sceneLayer.renderer = new SimpleRenderer({
+    symbol: new MeshSymbol3D({
+      symbolLayers: [
+        new FillSymbol3DLayer({
+          material: {
+            color: "#fefefe",
+            colorMixMode: "replace",
+          },
+          edges: new SolidEdges3D({
+            color: [255, 255, 255, 0.5],
+            size: 1,
+          }),
         }),
       ],
     }),
   });
 
-  view.map.add(pointLayer);
-
-  const formTemplate = new FormTemplate({
-    elements: [
-      new FieldElement({ fieldName: "ROTATION", label: "Rotation" }),
-      new FieldElement({ fieldName: "SIZE", label: "Size" }),
-    ],
+  onPlayClick("add-widget", () => {
+    const editor = new Editor({ view });
+    view.ui.add(editor, "top-right");
   });
-
-  const editor = new Editor({
-    view,
-    layerInfos: [{ layer: pointLayer, formTemplate }],
-  });
-
-  view.ui.add(editor, "top-right");
-}
+})();
