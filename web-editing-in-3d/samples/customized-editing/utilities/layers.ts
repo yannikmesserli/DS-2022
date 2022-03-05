7dd5238105a5e56526ef7a0bdd109d0131bb586d
Jesse van den Kieboom <jkieboom@esri.com>
2022-03-04 19:28:48 -0800

    Move creation of modifications layer down

--- web-editing-in-3d/samples/customized-editing/utilities/layers.ts
+++ web-editing-in-3d/samples/customized-editing/utilities/layers.ts
@@ -1,11 +1,11 @@
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
 import IntegratedMeshLayer from "@arcgis/core/layers/IntegratedMeshLayer";
 
-export const modificationsLayer = new GraphicsLayer();
-
 export const munichLayer = new IntegratedMeshLayer({
   portalItem: { id: "7a4ce32df4774997bbb32f230efbe7cc" },
 });
+
+export const modificationsLayer = new GraphicsLayer();
 
 export const floorsLayer = new GraphicsLayer();
 
