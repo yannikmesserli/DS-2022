fb829adb548187e00b6ca0ca838cd85e52b65877
Hugo Campos <hccampos@users.noreply.github.com>
2022-03-07 12:24:51 -0800

    Update airport terminal sample

--- web-editing-in-3d/samples/airport-terminal/airport-terminal.ts
+++ web-editing-in-3d/samples/airport-terminal/airport-terminal.ts
@@ -1,6 +1,7 @@
 import Camera from "@arcgis/core/Camera";
 import SpatialReference from "@arcgis/core/geometry/SpatialReference";
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer";
+import SolidEdges3D from "@arcgis/core/symbols/edges/SolidEdges3D";
 import ExtrudeSymbol3DLayer from "@arcgis/core/symbols/ExtrudeSymbol3DLayer";
 import PolygonSymbol3D from "@arcgis/core/symbols/PolygonSymbol3D";
 import SketchViewModel from "@arcgis/core/widgets/Sketch/SketchViewModel";
@@ -8,17 +9,19 @@ import { SAMEDAN_AIRPORT } from "../../../common/scenes";
 import { initView, onPlayClick } from "../../../common/utils";
 import { watchGraphic } from "../utils";
 
-const view = initView(SAMEDAN_AIRPORT);
-view.camera = new Camera({
-  position: {
-    spatialReference: SpatialReference.WebMercator,
-    x: 1099993.5949158794,
-    y: 5865684.589934241,
-    z: 1982.2895089341328,
-  },
-  heading: 300.363239318732,
-  tilt: 25.4903384824162,
-});
+const view = initView(
+  SAMEDAN_AIRPORT,
+  new Camera({
+    position: {
+      spatialReference: SpatialReference.WebMercator,
+      x: 1099993.5949158794,
+      y: 5865684.589934241,
+      z: 1982.2895089341328,
+    },
+    heading: 300.363239318732,
+    tilt: 25.4903384824162,
+  })
+);
 
 let terminals = new GraphicsLayer({
   elevationInfo: { mode: "absolute-height" },
@@ -43,7 +46,11 @@ onPlayClick("create", async () => {
     symbolLayers: [
       new ExtrudeSymbol3DLayer({
         size: 10,
-        material: { color: [255, 128, 0, 1] },
+        material: { color: [255, 255, 255, 1] },
+        edges: new SolidEdges3D({
+          size: 1,
+          color: [0, 0, 0, 1],
+        }),
       }),
     ],
   });
